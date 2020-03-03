5b53cb0e575a381a751301f5f6402321b3c6abe5
Jamie Davidson <jamie.davidson@red-gate.com>
2020-03-03 20:54:56 +0000

    Forgot to include mining component in Assignment class

--- src/main/java/assignment/Main.java
+++ src/main/java/assignment/Main.java
@@ -1,5 +1,6 @@
 package assignment;
 
+import assignment.components.MiningComponent;
 import assignment.components.MyInventoryComponent;
 import assignment.components.MyXPBoosterComponent;
 import net.gameslabs.model.Assignment;
@@ -9,7 +10,8 @@ public class Main {
     public static void main(String[] args) {
         new Assignment(
             new MyXPBoosterComponent(),
-            new MyInventoryComponent()
+            new MyInventoryComponent(),
+            new MiningComponent()
         ).run();
     }
 }
