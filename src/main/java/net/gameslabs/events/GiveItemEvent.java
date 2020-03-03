ccbf594141b7020891247fe375d24fed56c4a8c9
Jamie Davidson <jamie.davidson@red-gate.com>
2020-03-03 19:01:44 +0000

    Item class and GiveItemEvent

---
+++ src/main/java/net/gameslabs/events/GiveItemEvent.java
@@ -0,0 +1,15 @@
+package net.gameslabs.events;
+
+import net.gameslabs.api.Player;
+import net.gameslabs.api.PlayerEvent;
+
+public class GiveItemEvent extends PlayerEvent {
+    private final int id;
+    private final int amount;
+
+    public GiveItemEvent(Player player, int id, int amount) {
+        super(player);
+        this.id = id;
+        this.amount = amount;
+    }
+}
---
+++ src/main/java/net/gameslabs/model/Item.java
@@ -0,0 +1,11 @@
+package net.gameslabs.model;
+
+public class Item {
+    private int id;
+    private int amount;
+
+    public Item(int id, int amount) {
+        this.id = id;
+        this.amount = amount;
+    }
+}
