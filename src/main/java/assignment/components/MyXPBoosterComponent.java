a88512fa6f1df1435b50562a1a28007f45089ed9
Brandon <nardnob@users.noreply.github.com>
2020-10-29 14:49:02 -0500

    Initial commit. Add XP Multiplier functionality with MyXPBoosterComponent. Construction XP x2, Exploration XP x1

--- src/main/java/assignment/components/MyXPBoosterComponent.java
+++ src/main/java/assignment/components/MyXPBoosterComponent.java
@@ -2,8 +2,12 @@ package assignment.components;
 
 import net.gameslabs.api.Component;
 import net.gameslabs.events.GiveXpEvent;
+import net.gameslabs.model.Skill;
 
 public class MyXPBoosterComponent extends Component {
+    private static final double XP_MULTIPLIER_BASE = 1.0;
+    private static double XP_MULTIPLIER_CONSTRUCTION = 2.0;
+    private static double XP_MULTIPLIER_EXPLORATION = 1.0;
 
     @Override
     public void onLoad() {
@@ -11,11 +15,33 @@ public class MyXPBoosterComponent extends Component {
     }
 
     private void onGiveXP(GiveXpEvent event) {
-        // TODO: complete me
+        double xpMultiplier = 1.0;
+        switch(event.getSkill()) {
+            case CONSTRUCTION: xpMultiplier = this.XP_MULTIPLIER_CONSTRUCTION; break;
+            case EXPLORATION: xpMultiplier = this.XP_MULTIPLIER_EXPLORATION; break;
+        }
+
+        int finalXp = (int)Math.ceil(event.getXp() * this.XP_MULTIPLIER_BASE * xpMultiplier);
+        event.setXp(finalXp);
     }
 
     @Override
     public void onUnload() {
         // Do nothing
+    }
+
+    public double getXpMultiplier(Skill skill) {
+        switch(skill) {
+            case CONSTRUCTION: return this.XP_MULTIPLIER_CONSTRUCTION;
+            case EXPLORATION: return this.XP_MULTIPLIER_EXPLORATION;
+            default: return 0;
+        }
+    }
+
+    public void setXpMultiplier(Skill skill, double multiplier) {
+        switch(skill) {
+            case CONSTRUCTION: this.XP_MULTIPLIER_CONSTRUCTION = multiplier;
+            case EXPLORATION: this.XP_MULTIPLIER_EXPLORATION = multiplier;
+        }
     }
 }
